37bd6a2f68d32779b0c43ff2e0a2bbd661b0ba89
Arno Willig <arno.willig@hertz.st>
2024-02-03 16:10:02 +0100

    Added license info
    Signed-off-by: Arno Willig <arno.willig@hertz.st>

--- main.cpp
+++ main.cpp
@@ -1,3 +1,27 @@
+/*
+ * main.cpp
+ *
+ * Description: Demo for using the FrameClient component
+ *
+ * This file is part of qframeclient.
+ *
+ * qframeclient is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * qframeclient is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with qframeclient. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Author: Arno Willig
+ * Email: akw@thinkwiki.org
+ */
+
 #include "qframeclient.h"
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
